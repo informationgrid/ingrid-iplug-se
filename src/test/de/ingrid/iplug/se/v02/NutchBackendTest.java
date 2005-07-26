bac1339c718793bb211b5b6ce4b86fc7d505a2f4
hs <hs@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2005-07-26 14:42:27 +0000

    initial commit
    git-svn-id: https://harrison.its-technidata.de:444/svn/ingrid/ingrid-iplug-se/trunk@135 d5cb13e6-57fc-0310-9ab5-ddaedd36d86d

---
+++ src/test/de/ingrid/iplug/se/v02/NutchBackendTest.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 1997-2005 by media style GmbH
+ * 
+ * $Source: DispatcherTest.java,v $
+ */
+package de.ingrid.iplug.se.v02;
+
+import junit.framework.TestCase;
+
+/**
+ * Test for nutch-backend-feature (INGRID-14).
+ * created on 21.07.2005 <p>
+ *
+ * @author hs
+ */
+public class NutchBackendTest extends TestCase {
+
+	public void testPortNutchToJMX(){
+		fail("INGRID-84 not yet implemented");
+	}
+	
+	public void testStateMonitor(){
+		fail("INGRID-85 not yet implemented");
+	}
+	
+	public void testStartingAndStoppingOfProcessesManually(){
+		fail("INGRID-86 not yet implemented");
+	}
+	
+	public void testUrlTester(){
+		fail("INGRID-87 not yet implemented");
+	}
+	
+	public void testErrorProtocol(){
+		fail("INGRID-88 not yet implemented");
+	}
+	
+	public void testRegularExpressionConfiguration(){
+		fail("INGRID-89 not yet implemented");
+	}
+	
+	public void testNutchConfiguration(){
+		fail("INGRID-90 not yet implemented");
+	}
+	
+	public void testIndexFieldRanking(){
+		fail("INGRID-91 not yet implemented");
+	}
+}
