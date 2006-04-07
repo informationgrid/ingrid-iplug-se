069d90f967acc3f45d81dbf3cf08fa4035b0f64a
mb <mb@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2006-04-07 15:08:28 +0000

    add testCases
    git-svn-id: https://harrison.its-technidata.de:444/svn/ingrid/ingrid-iplug-se/trunk@2787 d5cb13e6-57fc-0310-9ab5-ddaedd36d86d

--- src/test/de/ingrid/iplug/se/TestNutchSearch.java
+++ src/test/de/ingrid/iplug/se/TestNutchSearch.java
@@ -13,21 +13,110 @@ import junit.framework.TestCase;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.nutch.util.NutchConfiguration;
 
+import de.ingrid.utils.IngridHit;
 import de.ingrid.utils.IngridHits;
+import de.ingrid.utils.query.ClauseQuery;
+import de.ingrid.utils.query.FieldQuery;
 import de.ingrid.utils.query.IngridQuery;
+import de.ingrid.utils.query.TermQuery;
 import de.ingrid.utils.queryparser.QueryStringParser;
 
 public class TestNutchSearch extends TestCase {
 
-    public void testSearch() throws Exception {
-        File file = new File("./testIndex");
-        Configuration conf = NutchConfiguration.create();
+  private Configuration fConfiguration;
+  private File fIndex;
 
-        conf.set("plugin.folders",
-                "/Users/joa23/Documents/workspace/nutch-trunk/src/plugin");
-        NutchSearcher searcher = new NutchSearcher(file, "testId", conf);
-        IngridQuery query = QueryStringParser.parse("partner:bund");
-        IngridHits hits = searcher.search(query, 0, 100);
-        assertTrue(hits.size() > 0);
+  protected void setUp() throws Exception {
+    this.fConfiguration = NutchConfiguration.create();
+    //this.fConfiguration.set("plugin.folders",
+    //"/Users/joa23/Documents/workspace/nutch-trunk/src/plugin");
+    this.fConfiguration.set("plugin.folders",
+        "/Users/mb/develop/workspace/nutch/src/plugin");
+    this.fConfiguration
+        .set(
+            "plugin.includes",
+            "protocol-http|urlfilter-regex|parse-(text|html|js)|index-basic|query-(basic|ingrid-se)");
+    //this.fIndex = new File("./testIndex");
+    this.fIndex = new File("/Users/mb/segments");
+  }
+
+  protected void tearDown() throws Exception {
+    this.fConfiguration.set("plugin.folders", "plugins");
+    this.fConfiguration
+        .set(
+            "plugin.includes",
+            "protocol-http|urlfilter-regex|parse-(text|html|js)|index-basic|query-(basic|site|url)");
+  }
+
+  /**
+   * @throws Exception
+   */
+  public void testSearch() throws Exception {
+    
+    NutchSearcher searcher = new NutchSearcher(this.fIndex, "testId", this.fConfiguration);
+    IngridQuery query = QueryStringParser.parse("partner:bund");
+    IngridHits hits = searcher.search(query, 0, 100);
+    assertTrue(hits.size() > 0);
+  }
+
+  
+
+  /**
+   * @throws Exception
+   */
+  public void testOrFieldQuery() throws Exception {
+    NutchSearcher searcher = new NutchSearcher(this.fIndex, "testId",
+        this.fConfiguration);
+
+    IngridQuery query = new IngridQuery();
+    query.addTerm(new TermQuery(true, false, "wasser"));
+    query.put("ranking", "date");
+    ClauseQuery cq = new ClauseQuery(true, false);
+    cq.addField(new FieldQuery(false, false, "topic", "gentechnik"));
+    cq.addField(new FieldQuery(false, false, "topic", "abfall"));
+   
+    query.addClause(cq);
+
+
+    IngridHits hits = searcher.search(query, 0, 100);
+    assertTrue(hits.length() > 0);
+    IngridHit[] hits2 = hits.getHits();
+    for (int i = 0; i < hits2.length; i++) {
+      IngridHit hit = hits2[i];
+      System.out.println(hit);
     }
+  }
+  
+  public void testOrFieldQuery2() throws Exception {
+
+    NutchSearcher searcher = new NutchSearcher(this.fIndex, "testId",
+        this.fConfiguration);
+
+    IngridQuery query = new IngridQuery();
+    query.addTerm(new TermQuery(true, false, "wasser"));
+    query.put("ranking", "date");
+    ClauseQuery cq = new ClauseQuery(true, false);
+    cq.addField(new FieldQuery(false, false, "topic", "gentechnik"));
+    cq.addField(new FieldQuery(false, false, "topic", "abfall"));
+   
+    
+    ClauseQuery cq2 = new ClauseQuery(false, false);
+    cq2.addField(new FieldQuery(true, false, "topic", "gentechnik2"));
+    cq2.addField(new FieldQuery(false, false, "topic", "abfall2"));
+   
+    cq.addClause(cq2);
+    query.addClause(cq);
+
+
+    IngridHits hits = searcher.search(query, 0, 100);
+    assertTrue(hits.length() > 0);
+    IngridHit[] hits2 = hits.getHits();
+    for (int i = 0; i < hits2.length; i++) {
+      IngridHit hit = hits2[i];
+      System.out.println(hit);
+    }
+  }
+  
+  
+
 }
