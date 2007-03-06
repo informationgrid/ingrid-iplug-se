32b216b2d68a90c7ea96c80b15ecdd560471338d
ms <ms@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2007-03-06 10:01:34 +0000

    o move of IndexIPlug to package de.ingrid.iplug.se
    git-svn-id: https://harrison.its-technidata.de:444/svn/ingrid/ingrid-iplug-se/trunk@6206 d5cb13e6-57fc-0310-9ab5-ddaedd36d86d

---
+++ src/java/de/ingrid/iplug/se/IndexIPlug.java
@@ -0,0 +1,31 @@
+package de.ingrid.iplug.se;
+
+import de.ingrid.utils.IPlug;
+import de.ingrid.utils.IngridHit;
+import de.ingrid.utils.IngridHitDetail;
+import de.ingrid.utils.IngridHits;
+import de.ingrid.utils.PlugDescription;
+import de.ingrid.utils.query.IngridQuery;
+
+public class IndexIPlug implements IPlug {
+
+    public void close() throws Exception {
+        // nothing todo
+    }
+
+    public void configure(PlugDescription arg0) throws Exception {
+        // nothing todo
+    }
+
+    public IngridHits search(IngridQuery arg0, int arg1, int arg2) throws Exception {
+        return null;
+    }
+
+    public IngridHitDetail getDetail(IngridHit arg0, IngridQuery arg1, String[] arg2) throws Exception {
+        return null;
+    }
+
+    public IngridHitDetail[] getDetails(IngridHit[] arg0, IngridQuery arg1, String[] arg2) throws Exception {
+        return null;
+    }
+}
