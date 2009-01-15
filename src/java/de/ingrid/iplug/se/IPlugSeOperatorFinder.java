5c9e8960229c699fc95ea846fd507af8c6709e45
mb <mb@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2009-01-15 13:52:22 +0000

    implement iplug-se operator (partner/provider) injector
    git-svn-id: https://harrison.its-technidata.de:444/svn/ingrid/ingrid-iplug-se/trunk@11006 d5cb13e6-57fc-0310-9ab5-ddaedd36d86d

--- src/java/de/ingrid/iplug/se/IPlugSeOperatorFinder.java
+++ src/java/de/ingrid/iplug/se/IPlugSeOperatorFinder.java
@@ -4,32 +4,86 @@ import java.io.File;
 import java.io.FileFilter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.logging.Logger;
 
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.LocalFileSystem;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.MultiReader;
+import org.apache.lucene.search.FieldCache;
 
-public class SearchableIndexFinder {
+import de.ingrid.utils.PlugDescription;
+import de.ingrid.utils.metadata.IPlugOperatorFinder;
 
-    private static final Logger LOG = Logger.getLogger(SearchableIndexFinder.class.getName());
-    
-    public List<File> findIndices(final FileSystem fileSystem, File folder) throws IOException {
+public class IPlugSeOperatorFinder implements IPlugOperatorFinder {
+
+    private static final Logger LOG = Logger.getLogger(IPlugSeOperatorFinder.class.getName());
+
+    private FileSystem _fileSystem = null;
+
+    private File _workinDirectory;
+
+    public List<File> findIndices(File folder) throws IOException {
+        _fileSystem = _fileSystem == null ? new LocalFileSystem(new Configuration()) : _fileSystem;
         List<File> indices = new ArrayList<File>();
         List<File> segments = new ArrayList<File>();
         List<File> allFolders = new ArrayList<File>();
 
-        findAllFolders(fileSystem, folder, allFolders);
+        findAllFolders(_fileSystem, folder, allFolders);
 
         for (File aFolder : allFolders) {
-            extractSegments(fileSystem, aFolder, segments);
+            extractSegments(_fileSystem, aFolder, segments);
         }
 
         for (File segment : segments) {
-            extractIndices(fileSystem, segment, indices);
+            extractIndices(_fileSystem, segment, indices);
         }
 
         return indices;
 
+    }
+
+    public void setFileSystem(FileSystem fileSystem) {
+        _fileSystem = fileSystem;
+    }
+
+    public FileSystem getFileSystem() {
+        return _fileSystem;
+    }
+
+    public Set<String> findIndexValues(File workingFolder, String indexFieldName) throws IOException {
+        List<File> indices = findIndices(workingFolder);
+        IndexReader[] readers = new IndexReader[indices.size()];
+        for (int i = 0; i < indices.size(); i++) {
+            readers[i] = IndexReader.open(indices.get(i));
+        }
+        MultiReader multiReader = new MultiReader(readers);
+        String[] values = FieldCache.DEFAULT.getStrings(multiReader, indexFieldName);
+        multiReader.close();
+
+        Set<String> valueSet = new HashSet<String>(Arrays.asList(values));
+        valueSet.remove(null);
+        return valueSet;
+    }
+
+    @Override
+    public Set<String> findPartner() throws IOException {
+        return findIndexValues(_workinDirectory, "partner");
+    }
+
+    @Override
+    public Set<String> findProvider() throws IOException {
+        return findIndexValues(_workinDirectory, "provider");
+    }
+
+    @Override
+    public void configure(PlugDescription plugDescription) {
+        _workinDirectory = plugDescription.getWorkinDirectory();
     }
 
     private void findAllFolders(final FileSystem fileSystem, File folder, List<File> list) {
