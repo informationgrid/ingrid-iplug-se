c520c0763ddb61e85cf5bd22c0157e12a8b25de8
mb <mb@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2009-05-24 07:21:12 +0000

    copy to opensource folder
    git-svn-id: https://harrison.its-technidata.de:444/svn/ingrid/ingrid-iplug-se/trunk@12244 d5cb13e6-57fc-0310-9ab5-ddaedd36d86d

---
+++ plugins/opensource/admin-crawl/src/java/org/apache/nutch/admin/crawl/CrawlCommand.java
@@ -0,0 +1,25 @@
+package org.apache.nutch.admin.crawl;
+
+public class CrawlCommand {
+
+  private Integer _depth;
+
+  private Integer _topn;
+
+  public Integer getDepth() {
+    return _depth;
+  }
+
+  public void setDepth(Integer depth) {
+    _depth = depth;
+  }
+
+  public Integer getTopn() {
+    return _topn;
+  }
+
+  public void setTopn(Integer topn) {
+    _topn = topn;
+  }
+
+}
