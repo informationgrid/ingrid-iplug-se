3b79c9235c7ce124b6bfb921d0953ccc9fde6164
mb <mb@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2009-07-21 14:05:49 +0000

    commit paging
    git-svn-id: https://harrison.its-technidata.de:444/svn/ingrid/ingrid-iplug-se/trunk@13344 d5cb13e6-57fc-0310-9ab5-ddaedd36d86d

--- plugins/closedsource/admin-urlmaintenance/src/java/de/ingrid/iplug/se/urlmaintenance/web/ListWebUrlsController.java
+++ plugins/closedsource/admin-urlmaintenance/src/java/de/ingrid/iplug/se/urlmaintenance/web/ListWebUrlsController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import de.ingrid.iplug.se.urlmaintenance.Paging;
 import de.ingrid.iplug.se.urlmaintenance.PartnerProviderCommand;
 import de.ingrid.iplug.se.urlmaintenance.commandObjects.StartUrlCommand;
 import de.ingrid.iplug.se.urlmaintenance.persistence.dao.IMetadataDao;
@@ -31,6 +32,7 @@ public class ListWebUrlsController {
   private final IStartUrlDao _startUrlDao;
   private final IProviderDao _providerDao;
   private final IMetadataDao _metadataDao;
+  private int _hitsPerPage = 10;
 
   @Autowired
   public ListWebUrlsController(IProviderDao providerDao,
@@ -43,7 +45,7 @@ public class ListWebUrlsController {
   @ModelAttribute("metadatas")
   public List<Metadata> injectMetadatas() {
     List<Metadata> arrayList = new ArrayList<Metadata>();
-    arrayList.add(_metadataDao.getByKeyAndValue("datatype", "topics"));
+    arrayList.add(_metadataDao.getByKeyAndValue("datatype", "www"));
     arrayList.add(_metadataDao.getByKeyAndValue("datatype", "research"));
     arrayList.add(_metadataDao.getByKeyAndValue("datatype", "law"));
     arrayList.add(_metadataDao.getByKeyAndValue("lang", "de"));
@@ -52,14 +54,18 @@ public class ListWebUrlsController {
   }
 
   @RequestMapping(value = "/listWebUrls.html", method = RequestMethod.GET)
-  public String listWebUrls( @ModelAttribute("partnerProviderCommand") PartnerProviderCommand partnerProviderCommand,
-      @RequestParam(value = "startIndex", required = false) Integer start,
-      @RequestParam(value = "pageSize", required = false) Integer length,
+  public String listWebUrls(
+      @ModelAttribute("partnerProviderCommand") PartnerProviderCommand partnerProviderCommand,
+      @RequestParam(value = "page", required = false) Integer page,
+      @RequestParam(value = "hitsPerPage", required = false) Integer hitsPerPage,
       @RequestParam(value = "sort", required = false) String sort,
       @RequestParam(value = "dir", required = false) String dir, Model model,
       HttpServletRequest request) {
-    start = start == null ? 0 : start;
-    length = length == null ? 10 : length;
+    page = page == null ? 1 : page;
+    hitsPerPage = hitsPerPage == null ? 10 : hitsPerPage;
+
+    // paging
+    int start = Paging.getStart(page, hitsPerPage);
 
     OrderBy orderBy = "url".equals(sort) ? ("asc".equals(dir) ? OrderBy.URL_ASC
         : OrderBy.URL_DESC) : ("asc".equals(dir) ? OrderBy.TIMESTAMP_ASC
@@ -70,9 +76,11 @@ public class ListWebUrlsController {
     if (byName != null) {
       count = _startUrlDao.countByProvider(byName);
       List<StartUrl> startUrls = _startUrlDao.getByProvider(byName, start,
-          length, orderBy);
+          hitsPerPage, orderBy);
       model.addAttribute("urls", startUrls);
     }
+    Paging paging = new Paging(10, hitsPerPage, count.intValue(), page);
+    model.addAttribute("paging", paging);
     return "web/listWebUrls";
   }
 
@@ -81,33 +89,34 @@ public class ListWebUrlsController {
     return new StartUrlCommand();
   }
 
-//  @RequestMapping(value = "/startUrlSubset.html", method = RequestMethod.GET)
-//  public String startUrlSubset(
-//      @ModelAttribute("partnerProviderCommand") PartnerProviderCommand partnerProviderCommand,
-//      @RequestParam(value = "startIndex", required = false) Integer start,
-//      @RequestParam(value = "pageSize", required = false) Integer length,
-//      @RequestParam(value = "sort", required = false) String sort,
-//      @RequestParam(value = "dir", required = false) String dir, Model model,
-//      HttpServletRequest request) {
-//    System.out.println(request.getParameterMap());
-//    start = start == null ? 0 : start;
-//    length = length == null ? 10 : length;
-//
-//    OrderBy orderBy = "url".equals(sort) ? ("asc".equals(dir) ? OrderBy.URL_ASC
-//        : OrderBy.URL_DESC) : ("asc".equals(dir) ? OrderBy.TIMESTAMP_ASC
-//        : OrderBy.TIMESTAMP_DESC);
-//    String providerString = partnerProviderCommand.getProvider();
-//    Provider byName = _providerDao.getByName(providerString);
-//    Long count = 0L;
-//    if (byName != null) {
-//      count = _startUrlDao.countByProvider(byName);
-//      List<StartUrl> startUrls = _startUrlDao.getByProvider(byName, start,
-//          length, orderBy);
-//      model.addAttribute("urls", startUrls);
-//    }
-//
-//    model.addAttribute("count", count);
-//
-//    return "web/startUrlSubset";
-//  }
+  // @RequestMapping(value = "/startUrlSubset.html", method = RequestMethod.GET)
+  // public String startUrlSubset(
+  // @ModelAttribute("partnerProviderCommand") PartnerProviderCommand
+  // partnerProviderCommand,
+  // @RequestParam(value = "startIndex", required = false) Integer start,
+  // @RequestParam(value = "pageSize", required = false) Integer length,
+  // @RequestParam(value = "sort", required = false) String sort,
+  // @RequestParam(value = "dir", required = false) String dir, Model model,
+  // HttpServletRequest request) {
+  // System.out.println(request.getParameterMap());
+  // start = start == null ? 0 : start;
+  // length = length == null ? 10 : length;
+  //
+  // OrderBy orderBy = "url".equals(sort) ? ("asc".equals(dir) ? OrderBy.URL_ASC
+  // : OrderBy.URL_DESC) : ("asc".equals(dir) ? OrderBy.TIMESTAMP_ASC
+  // : OrderBy.TIMESTAMP_DESC);
+  // String providerString = partnerProviderCommand.getProvider();
+  // Provider byName = _providerDao.getByName(providerString);
+  // Long count = 0L;
+  // if (byName != null) {
+  // count = _startUrlDao.countByProvider(byName);
+  // List<StartUrl> startUrls = _startUrlDao.getByProvider(byName, start,
+  // length, orderBy);
+  // model.addAttribute("urls", startUrls);
+  // }
+  //
+  // model.addAttribute("count", count);
+  //
+  // return "web/startUrlSubset";
+  // }
 }
