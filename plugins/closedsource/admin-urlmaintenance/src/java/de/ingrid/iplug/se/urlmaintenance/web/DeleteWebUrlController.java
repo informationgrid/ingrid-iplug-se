e181858fa1eaae369dd0b97bf453b32b0b29752e
mb <mb@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2009-07-27 12:18:23 +0000

    add logging
    git-svn-id: https://harrison.its-technidata.de:444/svn/ingrid/ingrid-iplug-se/trunk@13458 d5cb13e6-57fc-0310-9ab5-ddaedd36d86d

--- plugins/closedsource/admin-urlmaintenance/src/java/de/ingrid/iplug/se/urlmaintenance/web/DeleteWebUrlController.java
+++ plugins/closedsource/admin-urlmaintenance/src/java/de/ingrid/iplug/se/urlmaintenance/web/DeleteWebUrlController.java
@@ -2,6 +2,8 @@ package de.ingrid.iplug.se.urlmaintenance.web;
 
 import java.util.List;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,33 +20,39 @@ import de.ingrid.iplug.se.urlmaintenance.persistence.model.StartUrl;
 @Controller
 public class DeleteWebUrlController {
 
-
+  private static final Log LOG = LogFactory
+      .getLog(DeleteWebUrlController.class);
   private final IExcludeUrlDao _excludeUrlDao;
   private final ILimitUrlDao _limitUrlDao;
   private final IStartUrlDao _startUrlDao;
 
   @Autowired
-  public DeleteWebUrlController(IStartUrlDao startUrlDao, ILimitUrlDao limitUrlDao, IExcludeUrlDao excludeUrlDao) {
+  public DeleteWebUrlController(IStartUrlDao startUrlDao,
+      ILimitUrlDao limitUrlDao, IExcludeUrlDao excludeUrlDao) {
     _startUrlDao = startUrlDao;
     _limitUrlDao = limitUrlDao;
     _excludeUrlDao = excludeUrlDao;
   }
 
   @RequestMapping(method = RequestMethod.POST, value = "/deleteWebUrl.html")
-  public String deleteWebUrl(@RequestParam(value = "id", required = true) final Long id) {
+  public String deleteWebUrl(
+      @RequestParam(value = "id", required = true) final Long id) {
 
     StartUrl startUrl = _startUrlDao.getById(id);
     if (startUrl != null) {
       // TODO: check security user is allowed
       List<LimitUrl> limitUrls = startUrl.getLimitUrls();
       for (LimitUrl limitUrl : limitUrls) {
+        LOG.info("delete limit url: " + limitUrl);
         _limitUrlDao.makeTransient(limitUrl);
       }
-      
+
       List<ExcludeUrl> excludeUrls = startUrl.getExcludeUrls();
       for (ExcludeUrl excludeUrl : excludeUrls) {
+        LOG.info("delete exclude url: " + excludeUrl);
         _excludeUrlDao.makeTransient(excludeUrl);
       }
+      LOG.info("delete start url: " + startUrl);
       _startUrlDao.makeTransient(startUrl);
     }
 
