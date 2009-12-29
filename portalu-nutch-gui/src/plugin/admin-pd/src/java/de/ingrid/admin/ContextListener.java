9eb0a4e349ac9dba290cb58b80cc7199e800836f
rw.101tec <rw.101tec@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2009-12-29 12:33:37 +0000

    * added admin-pd into distribution * uncommented calling of ant build file in pom.xml
    git-svn-id: https://harrison.its-technidata.de:444/svn/ingrid/ingrid-iplug-se/trunk@14437 d5cb13e6-57fc-0310-9ab5-ddaedd36d86d

--- portalu-nutch-gui/src/plugin/admin-pd/src/java/de/ingrid/admin/ContextListener.java
+++ portalu-nutch-gui/src/plugin/admin-pd/src/java/de/ingrid/admin/ContextListener.java
@@ -1,5 +1,7 @@
 package de.ingrid.admin;
 
+import java.io.File;
+
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
@@ -10,20 +12,32 @@ public class ContextListener implements ServletContextListener {
     private static final Logger LOG = Logger.getLogger(ContextListener.class);
 
     public void contextInitialized(final ServletContextEvent contextEvent) {
+        final File confPath = new File("conf");
+        final File developConfPath = new File("portalu-nutch-gui/conf");
         // plug description
         String plugDescription = System.getProperty(IKeys.PLUG_DESCRIPTION);
-		if (plugDescription == null) {
-            plugDescription = "portalu-nutch-gui/conf/plugdescription-index.xml";
+        if (plugDescription == null) {
+            // test, if we are running in development or installation
+            // environment
+            if (developConfPath.isDirectory()) {
+                plugDescription = new File(developConfPath, "plugdescription-index.xml").getAbsolutePath();
+            } else {
+                plugDescription = new File(confPath, "plugdescription-index.xml").getAbsolutePath();
+            }
             System.setProperty(IKeys.PLUG_DESCRIPTION, plugDescription);
-            LOG.warn("plug description is not defined. using default: " + plugDescription);
+            LOG.info("plug description is not defined. using default: " + plugDescription);
         }
 
         // communication
         String communication = System.getProperty(IKeys.COMMUNICATION);
         if (communication == null) {
-            communication = "portalu-nutch-gui/conf/communication-index.xml";
+            if (developConfPath.isDirectory()) {
+                communication = new File(developConfPath, "communication-index.xml").getAbsolutePath();
+            } else {
+                communication = new File(confPath, "communication-index.xml").getAbsolutePath();
+            }
             System.setProperty(IKeys.COMMUNICATION, communication);
-            LOG.warn("commmunication is not defined. using default:" + communication);
+            LOG.info("commmunication is not defined. using default:" + communication);
         }
     }
 
