706a3de660eddffa80d80ae076784bd4b5fc15a6
jm <jm@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2010-03-18 19:04:59 +0000

    - attempt to decompress compressed report depending on the hadoop config
    THIS IS UNTESTED
    
    git-svn-id: https://harrison.its-technidata.de:444/svn/ingrid/ingrid-iplug-se/trunk@14806 d5cb13e6-57fc-0310-9ab5-ddaedd36d86d

--- 101tec-nutch-11e55b9/src/java/org/apache/nutch/mail/MailService.java
+++ 101tec-nutch-11e55b9/src/java/org/apache/nutch/mail/MailService.java
@@ -1,13 +1,22 @@
 package org.apache.nutch.mail;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.channels.FileChannel;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IOUtils;
+import org.apache.hadoop.io.compress.CompressionCodec;
+import org.apache.hadoop.io.compress.CompressionCodecFactory;
+import org.apache.hadoop.io.compress.CompressionInputStream;
 import org.apache.nutch.tools.UrlReporter;
 import org.apache.nutch.util.ZipUtil;
 
@@ -20,8 +29,11 @@ public class MailService {
     protected static MailService _instance;
 
     private final MailSender _mailSender;
+    
+    private final Configuration _configuration;
 
     private MailService(final Configuration configuration) {
+        _configuration = configuration;
         _mailSender = MailSender.get(configuration);
     }
 
@@ -46,6 +58,40 @@ public class MailService {
         LOG.debug("moving report file to local file");
         fileSystem.copyToLocalFile(report, reportPath);
         final File reportFile = new File(reportPath.toString());
+        
+        if (reportFile.isDirectory()) {
+            File[] listOfFiles = reportFile.listFiles();
+            CompressionCodecFactory ccf = new CompressionCodecFactory(_configuration);
+            for (int i=0; i<listOfFiles.length; i++) {
+                Path reportFilePart = new Path(reportPath, listOfFiles[i].getName());                
+                CompressionCodec codec = ccf.getCodec(reportFilePart);
+                String decompressedFileName = CompressionCodecFactory.removeSuffix(reportFilePart.getName(), codec.getDefaultExtension());
+
+                CompressionInputStream in = null;
+                OutputStream out = null;
+                File outf = null;
+                try {
+                    byte[] buffer = new byte[32000];
+                    int len;
+                    in = codec.createInputStream(new FileInputStream(listOfFiles[i]));
+                    outf = new File(decompressedFileName);
+                    out = new FileOutputStream(outf);
+                    while( 0 < (len = in.read(buffer)) ) {
+                        out.write( buffer, 0, len );
+                    }
+                } finally {
+                    if (in != null) {
+                       in.close();
+                    }
+                    if (out != null) {
+                       out.close();
+                    }
+                }
+                if (outf != null && outf.exists()) {
+                    listOfFiles[i].delete();
+                }
+            }
+        }
 
         // load attachment
         LOG.debug("creating attachment");
