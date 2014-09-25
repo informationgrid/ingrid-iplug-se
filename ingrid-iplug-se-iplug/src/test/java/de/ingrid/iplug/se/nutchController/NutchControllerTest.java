f2f28273072478aa22cdea174087f2981f6d6e98
jm <jm@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2014-09-25 10:34:10 +0000

    - add test for force-stop crawl process
    git-svn-id: https://harrison.its-technidata.de:444/svn/ingrid/ingrid-iplug-se/branches/3.5.x_new_architecture@24786 d5cb13e6-57fc-0310-9ab5-ddaedd36d86d

--- ingrid-iplug-se-iplug/src/test/java/de/ingrid/iplug/se/nutchController/NutchControllerTest.java
+++ ingrid-iplug-se-iplug/src/test/java/de/ingrid/iplug/se/nutchController/NutchControllerTest.java
@@ -81,5 +81,43 @@ public class NutchControllerTest {
 
         FileUtils.removeRecursive(Paths.get("test-instances"));
     }
+    
+    @Test
+    public void testForceStop() throws InterruptedException, IOException, JsonSyntaxException, JsonIOException, SAXException, ParserConfigurationException, TransformerException {
+        
+        FileUtils.removeRecursive(Paths.get("test-instances"));
+        
+        Configuration configuration = new Configuration();
+        configuration.setInstancesDir("test-instances");
+        SEIPlug.conf = configuration;
+        
+        Instance instance = new Instance();
+        instance.setName("test");
+        instance.setWorkingDirectory(SEIPlug.conf.getInstancesDir() + "/test");
+
+        Path conf = Paths.get(SEIPlug.conf.getInstancesDir(), "test", "conf").toAbsolutePath();
+        Path urls = Paths.get(SEIPlug.conf.getInstancesDir(), "test", "urls").toAbsolutePath();
+        Path logs = Paths.get(SEIPlug.conf.getInstancesDir(), "test", "logs").toAbsolutePath();
+        Files.createDirectories(logs);
+
+        FileUtils.copyDirectories(Paths.get("apache-nutch-runtime/runtime/local/conf").toAbsolutePath(), conf);
+        FileUtils.copyDirectories(Paths.get("../ingrid-iplug-se-nutch/src/test/resources/urls").toAbsolutePath(), urls);
+        FileUtils.copyDirectories(Paths.get("conf/default/conf").toAbsolutePath(), conf);
+        
+        IngridCrawlNutchProcess process = NutchProcessFactory.getIngridCrawlNutchProcess(instance, 1, 100);
+
+        NutchController nutchController = new NutchController();
+        nutchController.start(instance, process);
+        
+        Thread.sleep(5000);
+        assertEquals("Status is RUNNING", NutchProcess.STATUS.RUNNING, nutchController.getNutchProcess(instance).getStatus());
+        nutchController.stop(instance);
+        Thread.sleep(500);
+        assertEquals("Status is CANCELLED", NutchProcess.STATUS.INTERRUPTED, nutchController.getNutchProcess(instance).getStatus());
+
+        System.out.println(nutchController.getNutchProcess(instance).getStatusProvider().toString());
+
+        FileUtils.removeRecursive(Paths.get("test-instances"));
+    }    
 
 }
