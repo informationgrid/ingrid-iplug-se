220d52e4eae86d31fa3a6d5fa139b0f751cad5c3
jm <jm@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2014-10-09 19:08:04 +0000

    - select indexable metadata from database instead from metadata configuration
    git-svn-id: https://harrison.its-technidata.de:444/svn/ingrid/ingrid-iplug-se/branches/3.5.x_new_architecture@24896 d5cb13e6-57fc-0310-9ab5-ddaedd36d86d

--- ingrid-iplug-se-iplug/src/main/java/de/ingrid/iplug/se/nutchController/NutchProcessFactory.java
+++ ingrid-iplug-se-iplug/src/main/java/de/ingrid/iplug/se/nutchController/NutchProcessFactory.java
@@ -7,9 +7,10 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.TransformerException;
 
@@ -20,8 +21,7 @@ import com.google.gson.JsonIOException;
 import com.google.gson.JsonSyntaxException;
 
 import de.ingrid.iplug.se.SEIPlug;
-import de.ingrid.iplug.se.conf.UrlMaintenanceSettings.MetaElement;
-import de.ingrid.iplug.se.utils.InstanceConfigurationTool;
+import de.ingrid.iplug.se.db.DBManager;
 import de.ingrid.iplug.se.webapp.container.Instance;
 import edu.emory.mathcs.backport.java.util.Arrays;
 
@@ -33,8 +33,9 @@ import edu.emory.mathcs.backport.java.util.Arrays;
  */
 public class NutchProcessFactory {
 
-    //private final static Log log = LogFactory.getLog(NutchProcessFactory.class);
-    
+    // private final static Log log =
+    // LogFactory.getLog(NutchProcessFactory.class);
+
     /**
      * 
      * 
@@ -58,12 +59,11 @@ public class NutchProcessFactory {
         process.setWorkingDirectory(instance.getWorkingDirectory());
         process.addClassPath(Paths.get(instance.getWorkingDirectory(), "conf").toAbsolutePath().toString());
         // add default properties
+        process.addJavaOptions(new String[] { "-Dhadoop.log.dir=" + Paths.get(instance.getWorkingDirectory(), "logs").toAbsolutePath() });
         process.addJavaOptions(SEIPlug.conf.nutchCallJavaOptions.toArray(new String[] {}));
-        process.addJavaOptions(new String[] { "-Dhadoop.log.dir=" + Paths.get(instance.getWorkingDirectory(), "logs").toAbsolutePath()});
         process.addClassPath(Paths.get("apache-nutch-runtime/runtime/local").toAbsolutePath().toString());
-        process.addClassPath(Paths.get("apache-nutch-runtime","runtime","local","lib").toAbsolutePath().toString()
-                .concat(File.separator).concat("*"));
-        process.setStatusProvider(new StatusProvider( instance.getWorkingDirectory() ));
+        process.addClassPath(Paths.get("apache-nutch-runtime", "runtime", "local", "lib").toAbsolutePath().toString().concat(File.separator).concat("*"));
+        process.setStatusProvider(new StatusProvider(instance.getWorkingDirectory()));
 
         NutchConfigTool nutchConfigTool = new NutchConfigTool(Paths.get(instance.getWorkingDirectory(), "conf", "nutch-site.xml"));
 
@@ -74,11 +74,12 @@ public class NutchProcessFactory {
             metadataList.addAll(Arrays.asList(indexParseMdValue.split(",")));
         }
 
-        InstanceConfigurationTool instanceConfig = new InstanceConfigurationTool(Paths.get(instance.getWorkingDirectory(), "/conf/urlMaintenance.json")); 
-        for (Iterator<MetaElement> iter = instanceConfig.getMetadata().iterator(); iter.hasNext();) {
-            MetaElement mde = iter.next();
-            if (!metadataList.contains(mde.getId())) {
-                metadataList.add(mde.getId());
+        EntityManager em = DBManager.INSTANCE.getEntityManager();
+        TypedQuery<String> query = em.createQuery("select distinct md.metaKey from Metadata md", String.class);
+        List<String> resultList = query.getResultList();
+        for (String md : resultList) {
+            if (!metadataList.contains(md)) {
+                metadataList.add(md);
             }
         }
         indexParseMdValue = StringUtils.join(metadataList, ",");
@@ -87,11 +88,11 @@ public class NutchProcessFactory {
         nutchConfigTool.addOrUpdateProperty("hadoop.tmp.dir", Paths.get(instance.getWorkingDirectory(), "hadoop-tmp").toAbsolutePath().toString(), "Set hadoop temp directory to the instance.");
         nutchConfigTool.addOrUpdateProperty("mapred.temp.dir", Paths.get(instance.getWorkingDirectory(), "hadoop-tmp").toAbsolutePath().toString(), "Set mapred temp directory to the instance.");
         nutchConfigTool.addOrUpdateProperty("mapred.local.dir", Paths.get(instance.getWorkingDirectory(), "hadoop-tmp").toAbsolutePath().toString(), "Set mapred local directory to the instance.");
-        nutchConfigTool.addOrUpdateProperty("ingrid.indexer.elastic.type", instance.getName(), "Defines the index type of the indexed documents. The instance name will be used to be able to quickly manipulate all urls of an instance. This property only applies for the ingrid.indexer.elastic plugin.");
+        nutchConfigTool.addOrUpdateProperty("ingrid.indexer.elastic.type", instance.getName(),
+                "Defines the index type of the indexed documents. The instance name will be used to be able to quickly manipulate all urls of an instance. This property only applies for the ingrid.indexer.elastic plugin.");
         nutchConfigTool.addOrUpdateProperty("elastic.index", instance.getIndexName(), "Default index to send documents to.");
         nutchConfigTool.addOrUpdateProperty("elastic.port", instance.getEsTransportTcpPort(), "The port to connect to using TransportClient.");
-        nutchConfigTool.addOrUpdateProperty("elastic.host", instance.getEsHttpHost(), "The hostname to send documents to using TransportClient. Either host\n" + 
-        		"  and port must be defined or cluster.");
+        nutchConfigTool.addOrUpdateProperty("elastic.host", instance.getEsHttpHost(), "The hostname to send documents to using TransportClient. Either host\n" + "  and port must be defined or cluster.");
 
         nutchConfigTool.write();
 
